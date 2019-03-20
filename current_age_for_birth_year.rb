d80290256221728edeeb7b040376a86be7d4a3fa
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-20 14:32:20 +0000

    Done.

---
+++ current_age_for_birth_year.rb
@@ -0,0 +1,5 @@
+def current_age_for_birth_year(yearBorn)
+  age_of_person = 2003 - yearBorn
+end
+
+#Side Note: If I were to use this program in application, i would reference today's year using "Time.today.year"
